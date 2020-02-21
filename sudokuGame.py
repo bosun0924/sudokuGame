d3402f059aa782842e8789747869155b4e5a3482
Bo Sun <sunbo804400200@gmail.com>
2020-02-20 23:01:00 -0500

    try pygame

--- sudokuGame.py
+++ sudokuGame.py
@@ -98,5 +98,59 @@ for row in range(9):
     grid.append([])
     for column in range(9):
         grid[row].append(board[row][column])  # Append a cell
-print("Game starts")
-print(grid)
+# Initialize pygame
+pygame.init()
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [255, 255]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+ 
+# Set title of screen
+pygame.display.set_caption("Sudoku!")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+print("h")
+# -------- Main Program Loop -----------
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = pos[0] // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # Set that location to one
+            grid[row][column] = 1
+            print("Click ", pos, "Grid coordinates: ", row, column)
+ 
+    # Set the screen background
+    screen.fill(BLACK)
+ 
+    # Draw the grid
+    for row in range(10):
+        for column in range(10):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            pygame.draw.rect(screen,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+ 
+    # Limit to 60 frames per second
+    clock.tick(60)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
